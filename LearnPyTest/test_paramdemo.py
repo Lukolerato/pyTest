4ef80abc923788b032daa40e816254ad03326f40
Lukolerato <lioko.good@gmail.com>
2023-07-03 15:16:43 +0200

    @pytest.mark.parametrize

--- LearnPyTest/test_paramdemo.py
+++ LearnPyTest/test_paramdemo.py
@@ -1,15 +1,16 @@
-#writing methods to test
+# writing methods to test
 import pytest
 
-@pytest.fixture(params=["a","b"])
-def demo_fixture(request):
-    print(request.param)
+
+#
+# @pytest.fixture(params=["a","b"])
+# def demo_fixture(request):
+#     print(request.param)
 
 
-@pytest.mark.sanity
-def testLogin(demo_fixture):
-    print("Login Successful")
-
+@pytest.mark.parametrize("a,b,final", [(2, 6, 8), (5, 8, 15), (5, 10, 15)])
+def testAdd(a, b, final):
+    assert a + b == final
 
 # def testLogoff():
 #     print("Logoff Successful")
